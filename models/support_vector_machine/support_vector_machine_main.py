67db1032934a22b79add612b2278248f34d7c843
Marko Janosevic <markojanosevicsm7@gmail.com>
2023-07-02 18:57:17 +0200

    #5 Implemented custom support vector machine model

--- models/support_vector_machine/support_vector_machine_main.py
+++ models/support_vector_machine/support_vector_machine_main.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import StandardScaler
 
 import numpy as np
 
+from models.prophet_models.prophet_support_vector_machine import ProphetLinearSVM
 from shared.utils import fetch_api_data
 
 
@@ -23,11 +24,11 @@ def support_vector_machine_prediction(company_code, period):
     y_pred_low, mse_low = predict(x_train_low, x_test_low, y_train_low, y_test_low)
     y_pred_volume, mse_volume = predict(x_train_volume, x_test_volume, y_train_volume, y_test_volume)
 
-    prophet_y_pred_open, prophet_mse_open = predict(x_train_open, x_test_open, y_train_open, y_test_open)
-    prophet_y_pred_close, prophet_mse_close = predict(x_train_close, x_test_close, y_train_close, y_test_close)
-    prophet_y_pred_high, prophet_mse_high = predict(x_train_high, x_test_high, y_train_high, y_test_high)
-    prophet_y_pred_low, prophet_mse_low = predict(x_train_low, x_test_low, y_train_low, y_test_low)
-    prophet_y_pred_volume, prophet_mse_volume = predict(x_train_volume, x_test_volume, y_train_volume, y_test_volume)
+    prophet_y_pred_open, prophet_mse_open = prophet_predict(x_train_open, x_test_open, y_train_open, y_test_open)
+    prophet_y_pred_close, prophet_mse_close = prophet_predict(x_train_close, x_test_close, y_train_close, y_test_close)
+    prophet_y_pred_high, prophet_mse_high = prophet_predict(x_train_high, x_test_high, y_train_high, y_test_high)
+    prophet_y_pred_low, prophet_mse_low = prophet_predict(x_train_low, x_test_low, y_train_low, y_test_low)
+    prophet_y_pred_volume, prophet_mse_volume = prophet_predict(x_train_volume, x_test_volume, y_train_volume, y_test_volume)
 
     x_test_open = scaler_x_open.inverse_transform(x_test_open)
 
@@ -95,6 +96,14 @@ def support_vector_machine_prediction(company_code, period):
 
 def predict(x_train, x_test, y_train, y_test):
     model = LinearSVR()
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    mse = mean_squared_error(y_test, y_pred)
+    return y_pred.reshape(-1, 1), mse
+
+
+def prophet_predict(x_train, x_test, y_train, y_test):
+    model = ProphetLinearSVM()
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
     mse = mean_squared_error(y_test, y_pred)
