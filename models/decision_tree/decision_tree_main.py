3ab47bad996958c3119fe62e851b6acf2f3a9601
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2023-07-02 17:35:12 +0200

    #3 Implemented custom decision tree model

--- models/decision_tree/decision_tree_main.py
+++ models/decision_tree/decision_tree_main.py
@@ -1,3 +1,4 @@
+from models.prophet_models.prophet_decision_tree import MyDecisionTreeRegressor
 from shared.utils import fetch_api_data
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
@@ -20,42 +21,49 @@ def decision_tree_regression_prediction(company_code, period):
     y_pred_low, mae_low = predict(x_train_low, x_test_low, y_train_low, y_test_low)
     y_pred_volume, mae_volume = predict(x_train_volume, x_test_volume, y_train_volume, y_test_volume)
 
+    prophet_y_pred_open, prophet_mae_open = prophet_predict(x_train_open, x_test_open, y_train_open, y_test_open)
+    prophet_y_pred_close, prophet_mae_close = prophet_predict(x_train_close, x_test_close, y_train_close, y_test_close)
+    prophet_y_pred_high, prophet_mae_high = prophet_predict(x_train_high, x_test_high, y_train_high, y_test_high)
+    prophet_y_pred_low, prophet_mae_low = prophet_predict(x_train_low, x_test_low, y_train_low, y_test_low)
+    prophet_y_pred_volume, prophet_mae_volume = prophet_predict(x_train_volume, x_test_volume, y_train_volume,
+                                                                y_test_volume)
+
     response = {
         "companyCode": company_code,
         "period": period,
         "dates": x_test_open.flatten().tolist(),
         "open": {
-            "predictedCustom": y_pred_open.tolist(),
+            "predictedCustom": prophet_y_pred_open,
             "predictedLibrary": y_pred_open.tolist(),
-            "errorCustom": mae_open,
+            "errorCustom": prophet_mae_open,
             "errorLibrary": mae_open,
             "actual": y_test_open.tolist()
         },
         "close": {
-            "predictedCustom": y_pred_close.tolist(),
+            "predictedCustom": prophet_y_pred_close,
             "predictedLibrary": y_pred_close.tolist(),
-            "errorCustom": mae_close,
+            "errorCustom": prophet_mae_close,
             "errorLibrary": mae_close,
             "actual": y_test_close.tolist()
         },
         "high": {
-            "predictedCustom": y_pred_high.tolist(),
+            "predictedCustom": prophet_y_pred_high,
             "predictedLibrary": y_pred_high.tolist(),
-            "errorCustom": mae_high,
+            "errorCustom": prophet_mae_high,
             "errorLibrary": mae_high,
             "actual": y_test_high.tolist()
         },
         "low": {
-            "predictedCustom": y_pred_low.tolist(),
+            "predictedCustom": prophet_y_pred_low,
             "predictedLibrary": y_pred_low.tolist(),
-            "errorCustom": mae_low,
+            "errorCustom": prophet_mae_low,
             "errorLibrary": mae_low,
             "actual": y_test_low.tolist()
         },
         "volume": {
-            "predictedCustom": y_pred_volume.tolist(),
+            "predictedCustom": prophet_y_pred_volume,
             "predictedLibrary": y_pred_volume.tolist(),
-            "errorCustom": mae_volume,
+            "errorCustom": prophet_mae_volume,
             "errorLibrary": mae_volume,
             "actual": y_test_volume.tolist()
         },
@@ -66,6 +74,15 @@ def decision_tree_regression_prediction(company_code, period):
 
 def predict(x_train, x_test, y_train, y_test):
     model = DecisionTreeRegressor()
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    mae = mean_absolute_error(y_test, y_pred)
+
+    return y_pred, mae
+
+
+def prophet_predict(x_train, x_test, y_train, y_test):
+    model = MyDecisionTreeRegressor(max_depth=20)
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
     mae = mean_absolute_error(y_test, y_pred)
---
+++ models/prophet_models/prophet_decision_tree.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+
+class Node:
+    def __init__(self, feature_index=None, threshold=None, left=None, right=None, var_red=None, value=None):
+        self.feature_index = feature_index
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.var_red = var_red
+        self.value = value
+
+
+class MyDecisionTreeRegressor:
+    def __init__(self, min_samples_split=2, max_depth=2):
+        self.root = None
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+
+    def build_tree(self, dataset, curr_depth=0):
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        num_samples, num_features = np.shape(X)
+        best_split = {}
+        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
+            best_split = self.get_best_split(dataset, num_samples, num_features)
+            if best_split["var_red"]>0:
+                left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
+                right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
+                return Node(best_split["feature_index"], best_split["threshold"],
+                            left_subtree, right_subtree, best_split["var_red"])
+
+        leaf_value = self.calculate_leaf_value(Y)
+        return Node(value=leaf_value)
+
+    def get_best_split(self, dataset, num_samples, num_features):
+        best_split = {}
+        max_var_red = -float("inf")
+        for feature_index in range(num_features):
+            feature_values = dataset[:, feature_index]
+            possible_thresholds = np.unique(feature_values)
+            for threshold in possible_thresholds:
+                dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
+                if len(dataset_left)>0 and len(dataset_right)>0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    curr_var_red = self.variance_reduction(y, left_y, right_y)
+                    if curr_var_red>max_var_red:
+                        best_split["feature_index"] = feature_index
+                        best_split["threshold"] = threshold
+                        best_split["dataset_left"] = dataset_left
+                        best_split["dataset_right"] = dataset_right
+                        best_split["var_red"] = curr_var_red
+                        max_var_red = curr_var_red
+        return best_split
+
+    def split(self, dataset, feature_index, threshold):
+        dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
+        dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
+        return dataset_left, dataset_right
+
+    def variance_reduction(self, parent, l_child, r_child):
+        weight_l = len(l_child) / len(parent)
+        weight_r = len(r_child) / len(parent)
+        reduction = np.var(parent) - (weight_l*np.var(l_child) + weight_r*np.var(r_child))
+        return reduction
+
+    def calculate_leaf_value(self, Y):
+        val = np.mean(Y)
+        return val
+
+    def print_tree(self, tree=None, indent=" "):
+        if not tree:
+            tree = self.root
+
+        if tree.value is not None:
+            print(tree.value)
+
+        else:
+            print("X_"+str(tree.feature_index), "<=", tree.threshold, "?", tree.var_red)
+            print("%sleft:" % (indent), end="")
+            self.print_tree(tree.left, indent + indent)
+            print("%sright:" % (indent), end="")
+            self.print_tree(tree.right, indent + indent)
+
+    def fit(self, X, Y):
+        Y = np.reshape(Y, (-1, 1))
+        dataset = np.concatenate((X, Y), axis=1)
+        self.root = self.build_tree(dataset)
+
+    def make_prediction(self, x, tree):
+        if tree.value!=None: return tree.value
+        feature_val = x[tree.feature_index]
+        if feature_val<=tree.threshold:
+            return self.make_prediction(x, tree.left)
+        else:
+            return self.make_prediction(x, tree.right)
+
+    def predict(self, X):
+        predictions = [self.make_prediction(x, self.root) for x in X]
+        return predictions
